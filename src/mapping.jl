6b8703a350fa13dae69f4170a5952373181b027b
Beforerr <zzj956959688@gmail.com>
2024-11-13 09:10:04 -0800

    refactor: clean up mapping definitions and improve LaTeX string usage

--- src/mapping.jl
+++ src/mapping.jl
@@ -23,13 +23,6 @@ di_log_map = :ion_inertial_length => log10 => L"Log %$di_lab"
 jA_map = :j_Alfven => jA_lab
 jA_log_map = :j_Alfven => log10 => L"Log %$jA_lab"
 
-j_map = :j0_k => j_lab
-j_log_map = :j0_k => log10 => L"Log %$j_lab"
-j_norm_map = :j0_k_norm => j_norm_lab
-
-db_over_b_map = :db_over_b => L"|\Delta B|/\bar{B}"
-dB_over_B_map = :dB_over_B => L"\Delta B/\bar{B}"
-
 v_Alfven_map = "v.Alfven.change.l" => L"\Delta V_{A,l}"
 v_ion_map = "v.ion.change.l" => L"\Delta V_{i,l}"
 v_l_ratio_map = "v_l_ratio" => L"\Delta V_{i,l} / \Delta V_{A,l}"
@@ -38,21 +31,22 @@ function var_mapping()
 
     l_map = (;
         l=:L_n_cross => l_lab,
-        l_log=:L_n_cross => log10 => L"Log %$l_lab",
+        l_log=:L_n_cross => log10 => LaTeXString("Log $l_lab"),
         l_norm=:L_n_cross_norm => l_norm_lab,
         l_norm_log=:L_n_cross_norm => log10 => LaTeXString("Log $l_norm_lab"),
     )
 
     j_map = (;
         j=:j0_k => j_lab,
-        j_log=:j0_k => log10 => L"Log %$j_lab",
+        j_log=:j0_k => log10 => LatexString("Log $j_lab"),
         j_norm=:j0_k_norm => j_norm_lab,
         j_norm_log=:j0_k_norm => log10 => LaTeXString("Log $j_norm_lab"),
     )
 
     param_map = (;
+        dB_over_B=:dB_over_B => L"\Delta B / B",
         dB_norm_over_B=("dB_norm", "B.mean") => (/) => L"|\Delta \mathbf{B} |/B",
-        ω=:rotation_angle => "rotation angle",
+        ω=:ω => "rotation angle",
         ω_in=:ω_in => "in-plane rotation angle",
         bn=:bn_over_b => abs => L"B_N/B",
         jA=:j_Alfven => jA_lab,
